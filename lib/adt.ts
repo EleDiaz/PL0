3591e42e13bb9f82a8051c462e0c3da69da6586c
Eleazar <eleazardzdo@gmail.com>
2016-05-13 07:43:07 +0100

    añadiendo la semántica con clase

--- lib/adt.ts
+++ lib/adt.ts
@@ -70,7 +70,7 @@ class Var extends Base {
     }
 }
 
-class Function extends Base {
+class FunctionBlock extends Base {
     name : String
     params : [String]
     block : Block
@@ -85,33 +85,76 @@ class Function extends Base {
 // END Block
 
 // BEGIN Statament
-class Statement extends Base {
+class Compound extends Base {
+    statements : [Object]
+
+    constructor(location, statements) {
+      super(location)
+      this.statements = statements
+    }
 }
 
-class Assign extends Statement {
+class Assign extends Base {
+    name : String
+    value : Object
+
+    constructor(location, name, value) {
+      super(location)
+      this.name = name
+      this.value = value
+    }
 }
 
-class Call extends Statement {
+class IfThenElse extends Base {
+    cond : Object
+    thenSt : Object
+    elseSt : Object
+
+    constructor(location, cond, thenSt, elseSt) {
+      super(location)
+      this.cond = cond
+      this.thenSt = thenSt
+      this.elseSt = elseSt
+    }
 }
 
-class IfThenElse extends Statement {
+class While extends Base {
+    cond : Object
+    statement : Object
+
+    constructor(location, cond, statement) {
+      super(location)
+      this.cond = cond
+      this.statement = statement
+    }
 }
 
-class While extends Statement {
-}
-// END statement
-
-// BEGIN condition
-class Condition extends Base {
-}
-// END condition
-
-
-class Expresion extends Base {
+class Return extends Base {
+    returnSt : Object | void
+    constructor(location, returnSt) {
+      super(location)
+      this.returnSt = returnSt
+    }
 }
 
-class Term extends Base {
+class Operation extends Base {
+    operation : String
+    operands : [Operation]
+
+    constructor(location, operation, operands) {
+      super(location)
+      this.operation = operation
+      this.operands = operands
+    }
 }
 
-class Factor extends Base {
+class Literal extends Base {
+    value : Object
+    type : String
+
+    constructor(location, value, type) {
+      super(location)
+      this.value = value
+      this.type = type
+    }
 }
