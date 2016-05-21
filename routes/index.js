12bbeaa813b507687b2de07a45d2a977f1cc9bcc
alu0100845235 <alu0100845235@ull.edu.es>
2016-05-21 17:35:18 +0100

    new Routes created

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,26 @@
 var express = require('express');
 var passport = require('passport');
 var User = require('../model/user');
+var Code = require('../model/code');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { select: 'Features', user : req.user });
 });
 
-router.get('/register', function(req, res) {
+router.get('/register', (req, res)  => {
     res.render('register', { });
 });
 
-router.post('/register', function(req, res) {
-    User.register(new User({ username : req.body.username }), req.body.password, function(err, user) {
+router.post('/register', (req, res) => {
+    User.register(new User({ username : req.body.username }), req.body.password, (err, user) = > {
         if (err) {
             return res.render('register', { error : error.message });
         }
 
-        passport.authenticate('local')(req, res, function () {
-            req.session.save(function (err) {
+        passport.authenticate('local')(req, res, () => {
+            req.session.save( (err) => {
                 if (err) {
                     return next(err);
                 }
@@ -29,25 +30,53 @@ router.post('/register', function(req, res) {
     });
 });
 
-router.get('/login', function(req, res) {
+router.get('/login', (req, res) => {
     res.render('login', { user : req.user });
 });
 
-router.post('/login', passport.authenticate('local'), function(req, res) {
+router.post('/login', passport.authenticate('local'), (req, res) => {
     res.redirect('/');
 });
 
-router.get('/logout', function(req, res) {
+router.get('/logout', (req, res) => {
     req.logout();
     res.redirect('/');
 });
 
-router.get('/About', function(req, res, next) {
+router.get('/About', (req, res, next) => {
   res.render('about', { select: 'About', user : req.user})
 });
 
-router.get('/Examples', function(req, res, next) {
+router.get('/Examples', (req, res, next) => {
   res.render('compiler', { select: 'Examples', user : req.user})
+});
+
+router.get('/saveCode', (req, res) => {
+  let query = Code.saveCode (req.user, req.query.name, req.query.code);
+  Promise.all([query]).then((value) => {
+    res.send ({result: true});
+  });
+});
+
+router.get('/updateCode', (req, res) => {
+  let query = Code.updateCode (req.user, req.query.name, req.query.code);
+  Promise.all([query]).then((value) => {
+    res.send ({result: true});
+  });
+});
+
+router.get('/getCode', (req, res) => {
+  let query = Code.getCode (req.user, req.query.name);
+  Promise.all([query]).then((value) => {
+    res.send ({result: value});
+  });
+});
+
+router.get('/getListOfCodes', (req, res) => {
+  let query = Code.getListOfCodes (req.user);
+  Promise.all([query]).then((value) => {
+    res.send ({result: value});
+  });
 });
 
 module.exports = router;
