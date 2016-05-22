13e03e3fff7a5795ba2bf71b4dcb74bbe6af6729
Eleazar <eleazardzdo@gmail.com>
2016-05-22 22:40:50 +0100

    fixing preferences in pegs

--- model/compiler.js
+++ model/compiler.js
@@ -1,6 +1,7 @@
 "use strict";
 const util = require('util');
 const semantic = require('../lib/semantic');
+var Tracer = require('pegjs-backtrace');
 var PEG = require('../lib/pl0.js');
 exports.compileToAST = (text) => {
     try {
@@ -11,13 +12,15 @@ exports.compileToAST = (text) => {
     }
 };
 exports.compile = (text) => {
+    var tracer = new Tracer(text);
     try {
-        var r = PEG.parse(text);
+        var r = PEG.parse(text, {tracer: tracer});
         semantic.semantic(r);
-        return util.inspect(r, { depth: null });
-    }
-    catch (e) {
+        return util.inspect(r, {depth: null});
+    } catch (e) {
+        console.log(tracer.getBacktraceString());
+
+        console.log(`Error en línea ${e.location.start.line} columna ${e.location.start.column}`);
         console.log(e);
-        return e;
     }
 };
