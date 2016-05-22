39b9586e0e0a0f72fe335d4537f4d0791818f6af
Eleazar <eleazardzdo@gmail.com>
2016-05-22 11:30:50 +0100

    semi finished semi-ml? implementation

--- lib/type.ts
+++
@@ -1,9 +0,0 @@
-
-// Skecth de implementacion de un sistema de tipos
-
-class Type {
-	
-	constructor() {
-
-	}
-}
--- mainfromfile.js
+++ mainfromfile.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node --harmony_destructuring
+/*jshint esversion: 6 */
+/*jshint node:true */
 'use strict';
 
+var Tracer = require('pegjs-backtrace');
 var util = require('util');
 var fs = require('fs');
 var semantic = require('./lib/semantic.js');
@@ -8,16 +11,20 @@ var PEG = require("./lib/pl0.js");
 var fileName = process.argv[2] || 'input1.pl0';
 
 fs.readFile(fileName, 'utf8', function (err,input) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(`Processing <***\n${input}\n***>`);
- // try {
-    var r = PEG.parse(input);
-    //semantic.semantic(r);
-    console.log(util.inspect(r, {depth: null}));
- // } catch (e) {
- //   console.log(`Error en línea ${e.location.start.line} columna ${e.location.start.column}`);
- //   console.log(e);
- // }
+    if (err) {
+        return console.log(err);
+    }
+    console.log(`Processing <***\n${input}\n***>`);
+
+    var tracer = new Tracer(input);
+    // try {
+        var r = PEG.parse(input, {tracer: tracer});
+        //semantic.semantic(r);
+        console.log(util.inspect(r, {depth: null}));
+    // } catch (e) {
+    //     console.log(tracer.getBacktraceString());
+
+    //     console.log(`Error en línea ${e.location.start.line} columna ${e.location.start.column}`);
+    //     console.log(e);
+    // }
 });
