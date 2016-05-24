f1e6976272ba27e4c88f5e92e33bd9973698fa8a
Eleazar <eleazardzdo@gmail.com>
2016-05-24 20:58:29 +0100

    adding simplified test generation

--- test/testParser.js
+++ test/testParser.js
@@ -1,19 +1,27 @@
+/*jshint esversion: 6 */
+/*jshint node:true */
+
 "use strict";
+const fs = require('fs');
 const chai = require('chai');
 const ADT = require('../lib/adt');
 const compiler = require('../model/compiler');
 let expect = chai.expect;
+
+const CURRENT_DIR = "./test/testFiles/";
+
+const loadTest = () => {
+    let files = fs.readdirSync(CURRENT_DIR, 'utf8');
+
+    for (let file of files) {
+        let program = fs.readFileSync(CURRENT_DIR + file, 'utf8');
+        it('Testing `' + file + '`', () => {
+            expect(compiler.compileToAST(program)).instanceof(ADT.Program);
+        });
+    }
+};
+
+
 describe('Grammar', () => {
-    it('Should parse simple program', () => {
-        let program = `{}`;
-        let adt = compiler.compileToAST(program);
-        expect(adt).an.instanceof(ADT.Program);
-        expect(adt.name).eq('$main');
-        expect(adt.main).instanceof(ADT.Compound);
-    });
-    it('Parse a simple expresion', () => {
-        let program = `5+5`;
-        let adt = compiler.compileToAST(program);
-        expect(adt.main).an.instanceof(ADT.Operation);
-    });
+    loadTest();
 });
